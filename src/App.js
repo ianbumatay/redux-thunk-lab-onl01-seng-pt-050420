735b66858bbc96163dc6de4dd7fff9b2333db07f
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-02-15 23:04:59 -0500

    commit tree 389d5ddbe7f1de1daaa911c071382374458500b1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; 
 
-class App extends Component {   
+import { connect } from 'react-redux';
+import { fetchCats } from './actions/catActions'
+ 
+ 
+
+class App extends Component {    
+
+  componentDidMount() {
+    if (this.props.catPics.length === 0)
+      this.props.actions.fetchCats();
+  }
   
   render() {
+
+    console.log(this.props.catPics) // log will fire every time App renders 
+
     return (
       <div>
-        <h1>CatBook</h1>
+        <h1>CatBook</h1> 
+        <CatList catPics={this.props.catPics} />
         {/* add CatList component here */}
       </div>
     );
   }
-}
+} //
 
-export default App
+const mapStateToProps = state => {
+  return {
+    catPics: state.cats,
+    loading: state.loading
+  }
+}  
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchCats: () => dispatch(fetchCats())
+  }
+}  
+
+export const WrapperApp = connect(mapStateToProps, mapDispatchToProps)(App)
+
+//export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+//export default connect(mapStateToProps)(App)
+
+//export default App
 
